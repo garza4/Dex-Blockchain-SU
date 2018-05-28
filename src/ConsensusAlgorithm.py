d450d192508191f056c2da965d74b8b264dde95b
S Boyd <sboyd@Ss-MacBook-Air.local>
2018-05-28 15:59:02 -0500

    list of unique forgers is now picked randomly based on the CoinAge

--- src/ConsensusAlgorithm.py
+++ src/ConsensusAlgorithm.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """ TO DO
-* weight random forger selection
+* weight random forger selection DONE -- used random.choices method and replaced duplicates with new users
 * method to give transaction fees to chosen forger
 * method to check waiting stake holders and release funds as they become eligible
 
@@ -23,7 +23,7 @@ class Stake():
         self.__waitingStake = {} #{userID: [amount of stake, date and time of stake removal]}
         
     #Returns whether the given user is in the __stakeHolders dictionary or not
-    def __isStakeHolder(self, userID):
+    def __isInDictionary(self, userID):
         for key in self.__stakeHolders:
             if key == userID: #userID was found, return true and end function
                 return True
@@ -31,11 +31,11 @@ class Stake():
         
     #Adds the userID, amount of stake, and date/time to the dictionary of __stakeHolders
     def addStake(self, userID, amount):
-        self.__stakeHolders[userID] = [amount, datetime.datetime.now()]
+        self.__stakeHolders[userID] = [amount, datetime.datetime.now() - datetime.timedelta(days=31)]
     
     #Removes the userID form __stakeHolder and adds ID and amount to _waitingStake if user currently holds stake
     def removeStake(self, userID): 
-        if self.__isStakeHolder(userID):
+        if self.__isInDictionary(userID):
             self.__waitingStake[userID] = [self.__stakeHolders[userID][0], datetime.datetime.now()]
             print (self.__waitingStake)
             del self.__stakeHolders[userID]
@@ -44,7 +44,7 @@ class Stake():
     
     #Returns the amount of stake for the given user if user is currently a stake holder
     def getStakeAmount(self, userID):
-        if self.__isStakeHolder(userID):
+        if self.__isInDictionary(userID):
             return self.__stakeHolders[userID][0]
         else:
             return 'User ', userID, ' is not a stake holder'
@@ -55,34 +55,52 @@ class Stake():
         potentialForgers = [] #userIDs of stake holders who have held stake for more than 30 days
         forgerWeights = [] #coinAge for users in potentialForgers array (parallel arrays)
         currentTime = datetime.datetime.now() #time and date that we start picking forgers
+        totalCoinAge = 0
         
         #for each of the stake holders, if they have held stake for more than 29 days,
         #add their userID and coinAge to the parallel arrays
         for key in self.__stakeHolders:
-            daysInStake =  currentTime - self.__stakeHolders[key][1]
+            daysInStake = currentTime - self.__stakeHolders[key][1]
             if daysInStake.days >= 30:
                 totalDays = daysInStake.days
                 if totalDays > 90: #no more benefit day wise if stake held for more than 90 days
                     totalDays = 90
                 coinAge = self.__stakeHolders[key][0] * totalDays
+                totalCoinAge += coinAge
                 potentialForgers.append(key)
                 forgerWeights.append(coinAge)
-        forgers = random.sample(potentialForgers, x) #still need to figure out a way to take into account forger's coinAge
-        return forgers 
+
+        forgers = random.choices(population=potentialForgers, weights=forgerWeights, cum_weights=None,  k=x) #.choices() takes into account forger's coinAge but replaces after chosing
+        
+        #remove duplicate forger IDs from the list
+        #I didn't make this a private method because we need to have access to the potentialForgers and forgerWeights arrays
+        uniqueForgers = []
+        for x in forgers:
+            if x in uniqueForgers:
+                #find new stake holder
+                newForger = random.choices(population=potentialForgers, weights=forgerWeights, cum_weights=None,  k=1)
+                while newForger[0] in uniqueForgers:
+                    newForger = random.choices(population=potentialForgers, weights=forgerWeights, cum_weights=None,  k=1)
+                uniqueForgers.append(newForger[0])
+            else:
+                uniqueForgers.append(x)
+
+        return uniqueForgers
     
 class main():
     """ FOR TESTING
     stakeholders = Stake()
     stakeholders.addStake(1234, 300.00)
     stakeholders.addStake(5678, 9000.00)
-    print(stakeholders.getStakeAmount(1234))
-    print(stakeholders.getStakeAmount(5678))
-    #list = stakeholders.chooseStakeHolders(1)
-    #print(list)
+    stakeholders.addStake(9123, 40000.00)
+    #print(stakeholders.getStakeAmount(1234))
+    #print(stakeholders.getStakeAmount(5678))
+    list = stakeholders.chooseStakeHolders(2)
+    print(list)
     
-    stakeholders.removeStake(1235)
+    #stakeholders.removeStake(1235)
     #print (stakeholders.getStakeAmount(1234))
-    """
+     """
     
     
 main()
