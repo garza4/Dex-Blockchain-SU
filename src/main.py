705b541c4c433e71425da3717d73b9a3ed717520
garza4 <garza4@southwestern.edu>
2018-05-28 12:39:28 -0500

    setting up main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
 """ call graph and whatever else needs to be called here """
+import src.setup as setup
+import src.network.property as net
+import src.ConsensusAlgorithm as ca
+import CommandLineWallets as clw
+import networkx as nx
+
+print "Hello"
